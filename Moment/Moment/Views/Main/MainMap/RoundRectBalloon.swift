c8867e20dc7a6d076630c57b29948cfebb18b8d2
MJKim <qorhaqt@gmail.com>
2023-12-11 15:02:23 +0900

    Add: RoundRectBallon Custom Component - 2.2

--- Moment/Moment/Views/Main/MainMap/RoundRectBalloon.swift
+++ Moment/Moment/Views/Main/MainMap/RoundRectBalloon.swift
@@ -7,12 +7,55 @@
 
 import SwiftUI
 
-struct RoundRectBalloon: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct RoundRectBalloon: Shape {
+	func path(in rect: CGRect) -> Path {
+		var path = Path()
+		let radius = 10.0
+		
+		path.move(to: CGPoint(x: rect.minX + 10, y: rect.minY))
+		path.addLine(to: CGPoint(x: rect.maxX - 10, y: rect.minY))
+		
+		path.addRelativeArc(center: CGPoint(x: rect.maxX - 10, y: rect.minY + radius), radius: radius,
+							startAngle: Angle.degrees(-90), delta: Angle.degrees(90))
+		
+		path.move(to: CGPoint(x: rect.maxX, y: rect.minY + radius))
+		
+		path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - radius))
+		path.addRelativeArc(center: CGPoint(x: rect.maxX - radius, y: rect.maxY - radius), radius: radius,
+							startAngle: Angle.degrees(0), delta: Angle.degrees(90))
+		
+		path.addLine(to: CGPoint(x: rect.midX + radius, y: rect.maxY))
+		
+		path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY + 15))
+		path.addLine(to: CGPoint(x: rect.midX - radius, y: rect.maxY))
+		path.addRelativeArc(center: CGPoint(x: rect.minX + radius, y: rect.maxY - radius), radius: radius,
+							startAngle: Angle.degrees(90), delta: Angle.degrees(90))
+		
+		path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + 10))
+		path.addRelativeArc(center: CGPoint(x: rect.minX + radius, y: rect.minY + 10), radius: radius,
+							startAngle: Angle.degrees(-180), delta: Angle.degrees(90))
+		
+		return path
+	}
+}
+
+struct MarkerButton: View {
+	var body: some View {
+		VStack {
+			ZStack {
+				RoundRectBalloon()
+					.fill(.white)
+					.frame(width: 200, height: 200)
+				
+				Image("bonobono", bundle: nil)
+					.resizable()
+					.frame(width: 190, height: 190)
+					.clipShape(.rect(cornerRadius: 8))
+			}
+		}
+	}
 }
 
 #Preview {
-    RoundRectBalloon()
+	MarkerButton()
 }
