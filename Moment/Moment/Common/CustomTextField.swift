fdb4d00bba104f118cd390f6fecf63e96c4412f9
withseon <dlstjsvv@gmail.com>
2023-12-11 21:46:17 +0900

    Add: TextField Custom 추가 - 6.1

--- Moment/Moment/Common/CustomTextField.swift
+++ Moment/Moment/Common/CustomTextField.swift
@@ -5,4 +5,40 @@
 //  Created by 정인선 on 12/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    @State var testSentence = ""
+    
+    var body: some View {
+        VStack {
+            TextField("TextField Placeholder", text: $testSentence)
+                .textFieldStyle(BorderedTextFieldStyle())
+        }
+        .frame(height: 10)
+        .padding()
+    }
+}
+
+// MARK: - 텍스트필드 커스텀
+struct BorderedTextFieldStyle: TextFieldStyle {
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.lightBrown)
+                .foregroundStyle(.clear)
+            
+            // TextField
+            configuration
+                .font(.regular14)
+                .padding(10)
+        }
+        
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
