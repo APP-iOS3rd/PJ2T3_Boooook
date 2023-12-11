56060b02b906f9845b9e2f35ef7a71f0fb9a14b8
SAY-Hong <bobseahee@gmail.com>
2023-12-12 00:21:44 +0900

    Feat: ShelfToRecordListView와 동일하게 뷰 구성 - 4.1

--- Moment/Moment/Views/RecordList/View/MapToRecordListView.swift
+++ Moment/Moment/Views/RecordList/View/MapToRecordListView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct MapToRecordListView: View {
+    //MARK: 연도 어떻게 받아올지 고민해보기
+    var bookTitle: [String] = ["아메리칸 프로메테우스", "미움받을 용기"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                ForEach(0..<bookTitle.count) { index in
+                    VStack(alignment: .leading, spacing: -20) {
+                        Text(bookTitle[index])
+                            .foregroundColor(Color.darkBrown)
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal, 10)
+                            .background(Color.offBrown)
+                            .cornerRadius(10)
+                            .fixedSize(horizontal: false, vertical: false)
+                            .padding(20)
+                        ForEach(bookData) { bookdata in
+                            CustomListDivider()
+                            NavigationLink(destination: Text("테스트 이동 뷰입니다.")) {
+                                MapToRecordCell(bookdata: bookdata)
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
 #Preview {
     MapToRecordListView()
 }
+
