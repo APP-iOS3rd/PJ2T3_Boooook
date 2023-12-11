e0a072192291fc41c08b780961f0d1366fae9998
SAY-Hong <bobseahee@gmail.com>
2023-12-11 22:56:50 +0900

    Add: MapToRecordListView 파일 생성

---
+++ Moment/Moment/Views/RecordList/Cell/MapToRecordCell.swift
@@ -0,0 +1,18 @@
+//
+//  MapToRecordCell.swift
+//  Moment
+//
+//  Created by 홍세희 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct MapToRecordCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapToRecordCell()
+}
---
+++ Moment/Moment/Views/RecordList/View/MapToRecordListView.swift
@@ -0,0 +1,18 @@
+//
+//  MapToRecordListView.swift
+//  Moment
+//
+//  Created by 홍세희 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct MapToRecordListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapToRecordListView()
+}
