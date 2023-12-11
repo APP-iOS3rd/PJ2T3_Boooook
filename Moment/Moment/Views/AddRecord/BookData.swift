acf4086dc1c2c2b54c9aee5cd8ae89d57487b144
withseon <dlstjsvv@gmail.com>
2023-12-11 21:47:15 +0900

    Add: BookData 추가 - 6.1

--- Moment/Moment/Views/AddRecord/BookData.swift
+++ Moment/Moment/Views/AddRecord/BookData.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+// MARK: - Book Data
+struct BookData: Identifiable {
+    let id = UUID()
+    let title: String
+    let image: String
+    let author: String
+    
+    static let bookDummyData: BookData = BookData(title: "세이노의 가르침", image: "dummyBookImage01", author: "세이노")
+}
